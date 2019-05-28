f20ee9cf555b648f4862fed2aaa52f79b9958332
m2sar <m2sar@ppti-14-409-16.ufr-info-p6.jussieu.fr>
2019-05-28 11:36:47 +0200

    Initial Commit

---
+++ QuickWeather/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  QuickWeather
+//
+//  Created by m2sar on 28/05/2019.
+//  Copyright © 2019 m2sar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
